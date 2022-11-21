6fb9fd6f5dc2132fe38c69836c9c638ed6c54815
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-11-21 09:37:52 -0600

    ♻ refactor not found

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,11 @@
 import express from 'express'
-import path from 'path'
+
 import { HomeRouter } from '../routes/home.routes.js'
+
+import { notFoundMiddleware } from '../middlewares/notFound.middleware.js'
 
 export const app = express()
 
 app.use(HomeRouter)
 
-app.use((req, res) => {
-	res.status(404).sendFile(path.resolve('src/static/404.html'))
-})
+app.use(notFoundMiddleware)
