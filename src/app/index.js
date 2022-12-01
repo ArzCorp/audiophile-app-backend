9ae1bfbbea96407d23aa1b40eb23e77c7ce3799d
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-11-30 21:40:36 -0600

    💫 add middleware

--- src/app/index.js
+++ src/app/index.js
@@ -3,12 +3,15 @@ import express, { json, text } from 'express'
 import { HomeRouter } from '../routes/home.routes.js'
 import { notFoundMiddleware } from '../middlewares/notFound.middleware.js'
 import { testRouter } from '../routes/test.routes.js'
+import { logger } from '../middlewares/logger.middleware.js'
 
 export const app = express()
 
 app.use(text())
 
 app.use(json())
+
+app.use(logger)
 
 app.use(HomeRouter)
 
