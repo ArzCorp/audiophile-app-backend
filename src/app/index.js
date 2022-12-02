55a996322b14151a64b92ea2f752769afc7ba628
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-12-01 21:25:05 -0600

    🔥 removed files

--- src/app/index.js
+++ src/app/index.js
@@ -2,8 +2,6 @@ import express, { json, text } from 'express'
 
 import { HomeRouter } from '../routes/home.routes.js'
 import { notFoundMiddleware } from '../middlewares/notFound.middleware.js'
-import { testRouter } from '../routes/test.routes.js'
-import { logger } from '../middlewares/logger.middleware.js'
 
 export const app = express()
 
@@ -11,10 +9,6 @@ app.use(text())
 
 app.use(json())
 
-app.use(logger)
-
 app.use(HomeRouter)
-
-app.use(testRouter)
 
 app.use(notFoundMiddleware)
