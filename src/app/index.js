1cb765c07338d784c2d5f39793364fbdb60937b4
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-12-02 23:13:44 -0600

    ✨ add favicon

--- src/app/index.js
+++ src/app/index.js
@@ -8,6 +8,8 @@ export const app = express()
 app.set('case sensitive routing', true)
 app.set('message', { name: 'Osvaldo' })
 
+app.use(express.static('public'))
+
 app.use(text())
 
 app.use(json())
