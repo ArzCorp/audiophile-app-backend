fa490e37b06fefdfa1d65f1ffea89916dba012b6
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-12-02 22:54:56 -0600

    ✨ add global variables

--- src/app/index.js
+++ src/app/index.js
@@ -5,10 +5,15 @@ import { notFoundMiddleware } from '../middlewares/notFound.middleware.js'
 
 export const app = express()
 
+app.set('case sensitive routing', true)
+app.set('message', { name: 'Osvaldo' })
+
 app.use(text())
 
 app.use(json())
 
 app.use(HomeRouter)
+
+console.log(app.get('message'))
 
 app.use(notFoundMiddleware)
